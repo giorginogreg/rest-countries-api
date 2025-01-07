8526b748f16b3c74bf9aa71f02b3e686fef2af2c
Gregorio Giorgino (AlmaViva DigitalTech) <g.giorgino@almaviva.it>
2025-01-07 16:21:22 +0100

    feat: added navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { Theme, ThemeService } from './services/theme.service';
 import { Observable } from 'rxjs';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass',
 })
