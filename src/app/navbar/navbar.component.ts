9fb3d8485b30aeeb28633e22dfa929a740b3d33d
Gregorio Giorgino (AlmaViva DigitalTech) <g.giorgino@almaviva.it>
2025-01-07 17:42:15 +0100

    feat: icons substitution with fontAwesome package

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,17 +1,21 @@
 import { Component, inject } from '@angular/core';
 import { Theme, ThemeService } from '../services/theme.service';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faMoon } from '@fortawesome/free-solid-svg-icons';
+
 import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.sass',
 })
 export class NavbarComponent {
   private themeService: ThemeService;
   theme: Observable<Theme>;
+  faMoon = faMoon;
 
   constructor() {
     this.themeService = inject(ThemeService);
