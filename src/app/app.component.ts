8c22da5492e8922b481bc7f5c3639864f3412282
Gregorio Giorgino (AlmaViva DigitalTech) <g.giorgino@almaviva.it>
2025-01-07 15:52:15 +0100

    feat: added feature for toggling theme via service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Theme, ThemeService } from './services/theme.service';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.sass'
+  styleUrl: './app.component.sass',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rest-countries-api';
+  theme: Observable<Theme>;
+  private themeService: ThemeService;
+
+  constructor() {
+    this.themeService = inject(ThemeService);
+    this.theme = this.themeService.mode$;
+  }
+
+  ngOnInit(): void {
+    this.theme = this.themeService.mode$;
+  }
+
+  toggleTheme() {
+    this.themeService.toggleMode();
+  }
 }
