735f3c4de0c64321199dc2af4f731d8f95efb254
Gregorio Giorgino (AlmaViva DigitalTech) <g.giorgino@almaviva.it>
2025-01-08 10:08:42 +0100

    feat: fixed navbar style

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -4,11 +4,12 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faMoon } from '@fortawesome/free-solid-svg-icons';
 
 import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.sass',
 })
@@ -16,7 +17,7 @@ export class NavbarComponent {
   private themeService: ThemeService;
   theme: Observable<Theme>;
   faMoon = faMoon;
-
+  Theme = Theme;
   constructor() {
     this.themeService = inject(ThemeService);
     this.theme = this.themeService.mode$;
