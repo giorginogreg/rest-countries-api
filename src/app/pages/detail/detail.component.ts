6b6e1a67d7db9fe80c52b22785266c99263abf5c
Gregorio Giorgino (AlmaViva DigitalTech) <g.giorgino@almaviva.it>
2025-01-09 15:59:05 +0100

    style: added multiple style parts

--- src/app/pages/detail/detail.component.ts
+++ src/app/pages/detail/detail.component.ts
@@ -1,58 +1,66 @@
 import { Component, inject } from '@angular/core';
 import { ApiService } from '../../services/api.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 import { EMPTY, mergeMap, Observable, of, tap } from 'rxjs';
 import { Currency, Language } from '../../types/api';
-import { CommonModule } from '@angular/common';
+import { CommonModule, Location } from '@angular/common';
 import { faLeftLong } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 @Component({
-  selector: 'app-detail',
-  imports: [CommonModule, FontAwesomeModule],
-  templateUrl: './detail.component.html',
-  styleUrl: './detail.component.sass',
+    selector: 'app-detail',
+    imports: [CommonModule, FontAwesomeModule, RouterModule],
+    templateUrl: './detail.component.html',
+    styleUrl: './detail.component.sass',
 })
 export class DetailComponent {
-  country$: Observable<any> = EMPTY;
+    country$: Observable<any> = EMPTY;
 
-  apiService = inject(ApiService);
-  route = inject(ActivatedRoute);
+    apiService = inject(ApiService);
+    route = inject(ActivatedRoute);
+    _location: Location = inject(Location);
+    faLeftLong = faLeftLong;
 
-  faLeftLong = faLeftLong;
+    currentCountry: any;
+    borderCountries$: Observable<any> = EMPTY;
+    constructor() {
+        this.route.params.subscribe((params) => {
+            this.country$ = this.apiService
+                .getCountryByCca2(params['cca2'])
+                .pipe(
+                    tap((country) => console.log(country)),
+                    mergeMap((res) => {
+                        if (res.borders)
+                            this.borderCountries$ =
+                                this.apiService.getNameFromCountries(
+                                    res.borders
+                                );
+                        return of(res);
+                    })
+                );
+        });
+    }
 
-  currentCountry: any;
-  borderCountries$: Observable<any> = EMPTY;
-  constructor() {
-    this.route.params.subscribe((params) => {
-      this.country$ = this.apiService.getCountryByCca2(params['cca2']).pipe(
-        tap((country) => console.log(country)),
-        mergeMap((res) => {
-          this.borderCountries$ = this.apiService
-            .getNameFromCountries(res.borders)
-            .pipe();
-          return of(res);
-        })
-      );
-    });
-  }
+    ngOnInit(): void {}
 
-  ngOnInit(): void {}
+    displayCurrencies(currencies: Currency[]) {
+        return Object.values(currencies)
+            .map((currency) => currency.name)
+            .join(', ');
+    }
 
-  displayCurrencies(currencies: Currency[]) {
-    return Object.values(currencies)
-      .map((currency) => currency.name)
-      .join(', ');
-  }
+    displayLanguages(languages: Language[]) {
+        return Object.values(languages)
+            .map((language) => language)
+            .join(', ');
+    }
 
-  displayLanguages(languages: Language[]) {
-    return Object.values(languages)
-      .map((language) => language)
-      .join(', ');
-  }
+    getNativeName(localLanguages: any) {
+        return Object.values(localLanguages)
+            .map((language: any) => language.common)
+            .join(', ');
+    }
 
-  getNativeName(localLanguages: any) {
-    return Object.values(localLanguages)
-      .map((language: any) => language.common)
-      .join(', ');
-  }
+    back() {
+        this._location.back();
+    }
 }
